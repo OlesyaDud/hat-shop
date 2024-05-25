2366a879e9900bc0800801718843555b3c0e8938
lesya <wodadevelopment@gmail.com>
2024-05-25 00:28:05 -0400

    commit

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,25 +1,32 @@
-const ProductCard = ({changeBigProductImage, imgURL, bigProductImage }) => {
-    const handleClick =()=>{
-        if(bigProductImage !== imgURL.bigHat){
-            changeBigProductImage(imgURL.bigHat)
-        }
+const ProductCard = ({ changeBigProductImage, imgURL, bigProductImage }) => {
+  const handleClick = () => {
+    if (bigProductImage !== imgURL.bigHat) {
+      changeBigProductImage(imgURL.bigHat);
     }
+  };
 
   return (
-    <div className={`border-2 rounded-xl
-    ${bigProductImage === imgURL.bigHat ? 'bg-sky-100' : 'border-transparent'}
+    <div
+      className={`border-2 rounded-xl
+    ${
+      bigProductImage === imgURL.bigHat
+        ? "bg-transparent"
+        : "border-transparent"
+    }
     cursor-pointer max-sm:flex-1
     `}
-    onClick={handleClick}
+      onClick={handleClick}
     >
-      
-      <div className='flex justify-center items-center bg-sky-50 bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
-        <img src={imgURL.thumbnail} width={127} height={103}
-        className='object-cover'
+      <div className="flex justify-center items-center  bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgURL.thumbnail}
+          width={127}
+          height={103}
+          className="object-cover"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/sections/top-hero-section/TopHero.jsx
+++ src/sections/top-hero-section/TopHero.jsx
@@ -1,30 +1,37 @@
-import React,  {useState} from 'react'
-import Button from '../../components/button/Button'
-import { arrowRight } from '../../assets/icons'
-import { hats, statistics } from '../../constants'
-import  {bigHat1}  from '../../assets/images'
-import ProductCard from '../../components/product-card/ProductCard'
+import React, { useState } from "react";
+import Button from "../../components/button/Button";
+import { arrowRight } from "../../assets/icons";
+import { hats, statistics } from "../../constants";
+import { bigHat1 } from "../../assets/images";
+import ProductCard from "../../components/product-card/ProductCard";
 
 const TopHero = () => {
   const [bigProductImage, setBigProductImg] = useState(bigHat1);
 
   return (
-    <section 
-    id='home'
-    className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container '
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container "
     >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            Introducing
+          </span>
+          <span className="span inline-block mt-3 ">Our Newest </span> Product
+          Collection
+        </h1>
 
-    <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
-          <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
-            <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>Introducing</span>
-            <span className='span inline-block mt-3 '>Our Newest </span> Product Collection
-          </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Be the first to explore our latest product innovations!
+        </p>
 
-          <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>Be the first to explore our latest product innovations!</p>
-      
-
-        <Button label="Shop Now" iconURL={arrowRight} altText="arrow-right-icon" />
-{/* 
+        <Button
+          label="Shop Now"
+          iconURL={arrowRight}
+          altText="arrow-right-icon"
+        />
+        {/* 
         <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
           {statistics.map((stat) => (
             <div key={stat.label}>
@@ -33,33 +40,32 @@ const TopHero = () => {
             </div>
           ))}
         </div> */}
-    </div>
-
-    <div className='relative flex-1 flex justify-center items-center xl:min-h-screen bg-yellow-50 max-xl:py-40 bg-cover bg-center'>
-      <img 
-      src={bigProductImage}
-      alt='collection' 
-      width={610} 
-      height={500}
-      className='object-contain relative z-10'
-      />
-
-      <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
-        {hats.map((image, index) => (
-          <div key={index}>
-            <ProductCard 
-            index={index}
-              imgURL={image} 
-              changeBigProductImage={(hat) => setBigProductImg(hat)}
-              bigProductImage={bigProductImage}
-            />
-          </div>
-        ))}
       </div>
-    </div>
 
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-cover bg-center">
+        <img
+          src={bigProductImage}
+          alt="collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {hats.map((image, index) => (
+            <div key={index}>
+              <ProductCard
+                index={index}
+                imgURL={image}
+                changeBigProductImage={(hat) => setBigProductImg(hat)}
+                bigProductImage={bigProductImage}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default TopHero
+export default TopHero;
